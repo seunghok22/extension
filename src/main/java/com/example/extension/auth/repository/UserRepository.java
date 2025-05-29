315c2a82b11b254bdc8e8ffea553b3d0b10138a5
seunghok22 <seunghok22@gmail.com>
2025-05-29 15:51:49 +0900

    jwt auth base설정

---
+++ src/main/java/com/example/extension/auth/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.extension.auth.repository;
+
+import com.example.extension.auth.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    UserEntity findByUsername(String username);
+}
---
+++ src/main/java/com/example/extension/auth/service/CustomOAuth2UserService.java
@@ -0,0 +1,72 @@
+package com.example.extension.auth.service;
+
+import com.example.extension.auth.dto.CustomOAuth2User;
+import com.example.extension.auth.dto.GoogleResponse;
+import com.example.extension.auth.dto.OAuth2Response;
+import com.example.extension.auth.dto.UserDTO;
+import com.example.extension.auth.entity.UserEntity;
+import com.example.extension.auth.repository.UserRepository;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+    private final UserRepository userRepository;
+    public CustomOAuth2UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+
+        OAuth2Response oAuth2Response = null;
+        if (registrationId.equals("google")) {
+
+            oAuth2Response = new GoogleResponse(oAuth2User.getAttributes());
+        }
+        else {
+
+            return null;
+        }
+
+        String username = oAuth2Response.getProvider()+" "+oAuth2Response.getProviderId();
+        UserEntity existData = userRepository.findByUsername(username);
+
+        if (existData == null) {    //계정이 없으면 -> 생성
+
+            UserEntity userEntity = new UserEntity();
+            userEntity.setUsername(username);
+            userEntity.setEmail(oAuth2Response.getEmail());
+            userEntity.setName(oAuth2Response.getName());
+            userEntity.setRole("ROLE_USER");
+
+            userRepository.save(userEntity);
+
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUsername(username);
+            userDTO.setName(oAuth2Response.getName());
+            userDTO.setRole("ROLE_USER");
+
+            return new CustomOAuth2User(userDTO);
+        }
+        else {  //로그인
+
+            existData.setEmail(oAuth2Response.getEmail());
+            existData.setName(oAuth2Response.getName());
+
+            userRepository.save(existData);
+
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUsername(existData.getUsername());
+            userDTO.setName(oAuth2Response.getName());
+            userDTO.setRole(existData.getRole());
+
+            return new CustomOAuth2User(userDTO);
+        }
+    }
+
+}
